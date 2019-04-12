da442edb97ac22afdbd6bc041eac574e0c65d523
周航 <satroki@outlook.com>
2019-04-12 14:05:13 +0800

    1

--- Npgsql.Copy/MappingInfo.cs
+++ Npgsql.Copy/MappingInfo.cs
@@ -4,6 +4,8 @@ using System.Text;
 using NpgsqlTypes;
 using Npgsql;
 using System.Reflection;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore;
 
 namespace Npgsql.Copy
 {
@@ -15,5 +17,45 @@ namespace Npgsql.Copy
         public bool IsDbGenerated { get; set; }
         public PropertyInfo PropertyInfo { get; set; }
         public string PropertyName { get; set; }
+        public bool IsEnumProperty { get; set; }
+        public EnumMappingType EnumMappingType { get; set; }
+
+        public MappingInfo()
+        {
+
+        }
+
+        public MappingInfo(IProperty prop)
+        {
+            ColumnName = $"\"{prop.Relational().ColumnName}\"";
+            ColumnType = GetColumnType(prop);
+            IsDbGenerated = prop.ValueGenerated != ValueGenerated.Never;
+            IsPrimaryKey = prop.IsPrimaryKey();
+            PropertyInfo = prop.PropertyInfo;
+            PropertyName = prop.PropertyInfo.Name;
+            IsEnumProperty = prop.ClrType.IsEnum;
+            if (IsEnumProperty)
+            {
+                EnumMappingType = ColumnType.Contains("int")
+                    ? EnumMappingType.Int
+                    : EnumMappingType.String;
+            }
+        }
+
+        private string GetColumnType(IProperty property)
+        {
+            var type = property.Relational().ColumnType;
+            if (type.StartsWith("timestamp", StringComparison.OrdinalIgnoreCase))
+                return "timestamp";
+            if (type.StartsWith("time", StringComparison.OrdinalIgnoreCase))
+                return "time";
+            return type;
+        }
+    }
+
+    public enum EnumMappingType
+    {
+        Int,
+        String
     }
 }
--- Npgsql.Copy/NpgsqlCopyHelper.cs
+++ Npgsql.Copy/NpgsqlCopyHelper.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Npgsql;
+using System.Threading.Tasks;
 
 namespace Npgsql.Copy
 {
@@ -13,8 +12,9 @@ namespace Npgsql.Copy
         private readonly string tableName;
         private readonly string schema;
         private readonly List<MappingInfo> mappingList = new List<MappingInfo>();
+        private readonly bool autoTransactions;
 
-        public NpgsqlCopyHelper(DbContext context)
+        public NpgsqlCopyHelper(DbContext context, bool autoTransactions = true)
         {
             var type = context.Model.FindEntityType(typeof(T));
             var r = type.Relational();
@@ -23,27 +23,45 @@ namespace Npgsql.Copy
             var props = type.GetProperties();
             foreach (var prop in props)
             {
-                var mi = new MappingInfo
-                {
-                    ColumnName = $"\"{prop.Relational().ColumnName}\"",
-                    ColumnType = prop.Relational().ColumnType,
-                    IsDbGenerated = prop.ValueGenerated != ValueGenerated.Never,
-                    IsPrimaryKey = prop.IsPrimaryKey(),
-                    PropertyInfo = prop.PropertyInfo,
-                    PropertyName = prop.PropertyInfo.Name,
-                };
+                var mi = new MappingInfo(prop);
                 mappingList.Add(mi);
+            }
+
+            this.autoTransactions = autoTransactions;
+        }
+
+        public int Insert(NpgsqlConnection conn, IEnumerable<T> list, NpgsqlTransaction tran = null)
+        {
+            if (list == null || !list.Any())
+                return 0;
+            var mis = mappingList.Where(mi => !mi.IsDbGenerated).ToList();
+            using (tran = GetTransaction(conn, tran))
+            {
+                try
+                {
+                    var count = CopyData(conn, list, mis, tableName);
+                    tran?.Commit();
+                    return count;
+                }
+                catch (Exception e)
+                {
+                    tran?.Rollback();
+                    throw e;
+                }
             }
         }
 
-        public void Insert(NpgsqlConnection conn, IEnumerable<T> list)
+        public Task<int> InsertAsync(NpgsqlConnection conn, IEnumerable<T> list, NpgsqlTransaction tran = null)
         {
-            var mis = mappingList.Where(mi => !mi.IsDbGenerated).ToList();
-            CopyData(conn, list, mis, tableName);
+            if (list == null || !list.Any())
+                return Task.FromResult(0);
+            return Task.Run(() => Insert(conn, list, tran));
         }
 
-        public int Update(NpgsqlConnection conn, IEnumerable<T> list, NpgsqlTransaction tran, IEnumerable<string> updateFields = null)
+        public int Update(NpgsqlConnection conn, IEnumerable<T> list, NpgsqlTransaction tran = null, IEnumerable<string> updateFields = null)
         {
+            if (list == null || !list.Any())
+                return 0;
             var mappings = mappingList;
             if (updateFields != null && updateFields.Any())
             {
@@ -54,39 +72,104 @@ namespace Npgsql.Copy
                         mappings.Add(mi);
                 }
             }
+            using (tran = GetTransaction(conn, tran))
+            {
+                try
+                {
+                    var temp = $"temp_{tableName.Trim('"').ToLower()}_{DateTime.Now.Ticks}";
+                    var sql = $"CREATE TEMP TABLE {temp} (LIKE {tableName} INCLUDING DEFAULTS) ON COMMIT DROP";
+                    new NpgsqlCommand(sql, conn, tran).ExecuteNonQuery();
 
-            var temp = $"temp_{tableName.Trim('"').ToLower()}_{DateTime.Now.Ticks}";
-            var sql = $"CREATE TEMP TABLE {temp} (LIKE {tableName} INCLUDING DEFAULTS) ON COMMIT DROP";
-            new NpgsqlCommand(sql, conn, tran).ExecuteNonQuery();
+                    CopyData(conn, list, mappings, temp);
 
-            CopyData(conn, list, mappings, temp);
-
-            var mis = mappings.Where(m => !m.IsPrimaryKey).ToList();
-            var updateCols = string.Join(",", mis.Select(m => $"{m.ColumnName} = source.{m.ColumnName}"));
-            var pk = mappings.First(m => m.IsPrimaryKey).ColumnName;
-            var updateSql = $"UPDATE {tableName} SET {updateCols} FROM {temp} as source WHERE {tableName}.{pk}=source.{pk}";
-            return new NpgsqlCommand(updateSql, conn, tran).ExecuteNonQuery();
+                    var mis = mappings.Where(m => !m.IsPrimaryKey).ToList();
+                    var updateCols = string.Join(",", mis.Select(m => $"{m.ColumnName} = source.{m.ColumnName}"));
+                    var pk = mappings.First(m => m.IsPrimaryKey).ColumnName;
+                    var updateSql = $"UPDATE {tableName} SET {updateCols} FROM {temp} as source WHERE {tableName}.{pk}=source.{pk}";
+                    var count = new NpgsqlCommand(updateSql, conn, tran).ExecuteNonQuery();
+                    tran?.Commit();
+                    return count;
+                }
+                catch (Exception e)
+                {
+                    tran?.Rollback();
+                    throw e;
+                }
+            }
         }
 
-        private static void CopyData(NpgsqlConnection conn, IEnumerable<T> list, List<MappingInfo> mis, string table)
+        public Task<int> UpdateAsync(NpgsqlConnection conn, IEnumerable<T> list, NpgsqlTransaction tran = null, IEnumerable<string> updateFields = null)
+        {
+            if (list == null || !list.Any())
+                return Task.FromResult(0);
+            return Task.Run(() => Update(conn, list, tran, updateFields));
+        }
+
+        private int CopyData(NpgsqlConnection conn, IEnumerable<T> list, List<MappingInfo> mis, string table)
         {
             var cols = string.Join(",", mis.Select(m => m.ColumnName));
+            var count = 0;
             using (var importer = conn.BeginBinaryImport($"COPY {table} ({cols}) FROM STDIN (FORMAT BINARY)"))
             {
                 foreach (var i in list)
                 {
                     importer.StartRow();
+                    count++;
                     foreach (var p in mis)
                     {
                         var v = p.PropertyInfo.GetValue(i);
                         if (v == null)
                             importer.WriteNull();
                         else
-                            importer.Write(v, p.ColumnType);
+                        {
+                            if (p.IsEnumProperty)
+                            {
+                                switch (p.EnumMappingType)
+                                {
+                                    case EnumMappingType.Int:
+                                        importer.Write((int)v, p.ColumnType);
+                                        break;
+                                    case EnumMappingType.String:
+                                        importer.Write(v.ToString(), p.ColumnType);
+                                        break;
+                                }
+                            }
+                            else
+                            {
+                                switch (v)
+                                {
+                                    case DateTimeOffset dt:
+                                        importer.Write(dt.DateTime, p.ColumnType);
+                                        break;
+                                    default:
+                                        importer.Write(v, p.ColumnType);
+                                        break;
+                                }
+                            }
+                        }
                     }
                 }
                 importer.Complete();
             }
+            return count;
+        }
+
+        private void EnsureConnectionOpened(NpgsqlConnection conn)
+        {
+            if (conn.State != System.Data.ConnectionState.Open)
+            {
+                conn.Open();
+            }
+        }
+
+        private NpgsqlTransaction GetTransaction(NpgsqlConnection conn, NpgsqlTransaction tran)
+        {
+            EnsureConnectionOpened(conn);
+            if (tran == null && autoTransactions)
+            {
+                return conn.BeginTransaction();
+            }
+            return tran;
         }
     }
 }
