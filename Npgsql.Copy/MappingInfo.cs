b6d88c7a321f71189d170a7e05f207e5fc07aef2
周航 <satroki@outlook.com>
2021-04-10 00:51:30 +0800

    5.0

--- Npgsql.Copy/MappingInfo.cs
+++ Npgsql.Copy/MappingInfo.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using NpgsqlTypes;
-using Npgsql;
-using System.Reflection;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Npgsql.Copy
 {
